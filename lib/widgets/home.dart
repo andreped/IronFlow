9c2a6b1c8158dbb0e724680411e4a875d97f605d
André Pedersen <andrped94@gmail.com>
2024-07-14 17:17:06 +0200

    Implemented simple functionality

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,94 +1,94 @@
 import 'package:flutter/material.dart';
 
-
-class HomePage extends StatefulWidget {
-  HomePage({key});
-
+class VariableStoreApp extends StatelessWidget {
   @override
-  _HomePageState createState() => new _HomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IronFlow',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: VariableStoreHomePage(),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
+class VariableStoreHomePage extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    _tabController = new TabController(vsync: this, length: 6);
-  }
+  _VariableStoreHomePageState createState() => _VariableStoreHomePageState();
+}
 
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
+class _VariableStoreHomePageState extends State<VariableStoreHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _variableNameController = TextEditingController();
+  final _variableValueController = TextEditingController();
+  final Map<String, String> _variables = {};
+
+  void _addVariable() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _variables[_variableNameController.text] = _variableValueController.text;
+        _variableNameController.clear();
+        _variableValueController.clear();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var style = TextStyle(fontSize: 35, color: Colors.white);
-    double height = MediaQuery.of(context).size.height;
-
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 6,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              controller: _tabController,
-              tabs: const [
-                Tab(icon: Icon(Icons.home)),
-              ],
-            ),
-            title: const Text('IronFlow'),
-            backgroundColor: Theme.of(context).colorScheme.secondary,
-          ),
-          body: TabBarView(
-            controller: _tabController,
-            children: [
-              Container(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 20,
-                        width: 20,
-                      ),
-                        SizedBox(
-                          height: height * 0.16, //height of button
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              _tabController.animateTo(2);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context).colorScheme.primary,
-                            ),
-                            child: Container(
-                              child: RichText(
-                                  text: TextSpan(
-                                    text: 'Show AI Model\n ',
-                                    style: style,
-                                    children: const <TextSpan>[
-                                      TextSpan(
-                                          text:
-                                              'Some data',
-                                          style: TextStyle(
-                                              fontSize: 15, color: Colors.white)),
-                                    ],
-                                  ),
-                                  textAlign: TextAlign.center),
-                            ),
-                          ),
-                        ),
-                    ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IronFlow'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _variableNameController,
+                    decoration: InputDecoration(labelText: 'Training exercise'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter information on exercise';
+                      }
+                      return null;
+                    },
                   ),
-                )
+                  TextFormField(
+                    controller: _variableValueController,
+                    decoration: InputDecoration(labelText: 'Variable Value'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter information on exercise';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: _addVariable,
+                    child: Text('Add Exercise'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _variables.length,
+                itemBuilder: (context, index) {
+                  String key = _variables.keys.elementAt(index);
+                  return ListTile(
+                    title: Text('$key: ${_variables[key]}'),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
