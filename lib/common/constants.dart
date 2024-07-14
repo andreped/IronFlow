a7863c9a012f2fc9869960c36978b53e3877f12e
André Pedersen <andrped94@gmail.com>
2024-07-14 20:14:33 +0200

    Moved constants to common

---
+++ lib/common/constants.dart
@@ -0,0 +1,12 @@
+const List<String> predefinedExercises = [
+  "Benchpress",
+  "Pull-up",
+  "Squats",
+  "Curls (z-bar)",
+  "Curls (manuals)",
+  "Deadlift",
+  "Leg extension",
+  "Hamstring curl"
+];
+
+const int dropdownVisibleItemCount = 5;
