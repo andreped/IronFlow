efcea38a101ce62c435d15561b840d44ad149472
André Pedersen <andrped94@gmail.com>
2024-07-14 17:39:09 +0200

    Implemented data storage, deletion, and visualization

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'database.dart';
+import 'visualization.dart';
 
 class VariableStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'IronFlow',
+      title: 'Variable Store App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -22,72 +24,122 @@ class _VariableStoreHomePageState extends State<VariableStoreHomePage> {
   final _formKey = GlobalKey<FormState>();
   final _variableNameController = TextEditingController();
   final _variableValueController = TextEditingController();
-  final Map<String, String> _variables = {};
+  final DatabaseHelper _dbHelper = DatabaseHelper();
 
-  void _addVariable() {
+  Future<void> _addVariable() async {
     if (_formKey.currentState!.validate()) {
-      setState(() {
-        _variables[_variableNameController.text] = _variableValueController.text;
-        _variableNameController.clear();
-        _variableValueController.clear();
-      });
+      await _dbHelper.insertVariable(
+        _variableNameController.text,
+        _variableValueController.text,
+      );
+      _variableNameController.clear();
+      _variableValueController.clear();
+      setState(() {});
     }
+  }
+
+  Future<void> _clearDatabase() async {
+    await _dbHelper.clearDatabase();
+    setState(() {});
+  }
+
+  Future<List<Map<String, dynamic>>> _getVariables() async {
+    return await _dbHelper.getVariables();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('IronFlow'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Variable Store App'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () async {
+                await _clearDatabase();
+              },
+            ),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.add), text: 'Add Variable'),
+              Tab(icon: Icon(Icons.table_chart), text: 'View Table'),
+              Tab(icon: Icon(Icons.show_chart), text: 'Visualize Data'),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            Form(
-              key: _formKey,
+            Padding(
+              padding: EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  TextFormField(
-                    controller: _variableNameController,
-                    decoration: InputDecoration(labelText: 'Training exercise'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter information on exercise';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _variableValueController,
-                    decoration: InputDecoration(labelText: 'Variable Value'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter information on exercise';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: _addVariable,
-                    child: Text('Add Exercise'),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _variableNameController,
+                          decoration: InputDecoration(labelText: 'Variable Name'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a variable name';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _variableValueController,
+                          decoration: InputDecoration(labelText: 'Variable Value'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a variable value';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: _addVariable,
+                          child: Text('Add Variable'),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _variables.length,
-                itemBuilder: (context, index) {
-                  String key = _variables.keys.elementAt(index);
-                  return ListTile(
-                    title: Text('$key: ${_variables[key]}'),
-                  );
-                },
-              ),
+            FutureBuilder<List<Map<String, dynamic>>>(
+              future: _getVariables(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                final variables = snapshot.data!;
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columns: [
+                      DataColumn(label: Text('ID')),
+                      DataColumn(label: Text('Name')),
+                      DataColumn(label: Text('Value')),
+                      DataColumn(label: Text('Timestamp')),
+                    ],
+                    rows: variables.map((variable) {
+                      return DataRow(cells: [
+                        DataCell(Text(variable['id'].toString())),
+                        DataCell(Text(variable['name'])),
+                        DataCell(Text(variable['value'])),
+                        DataCell(Text(variable['timestamp'])),
+                      ]);
+                    }).toList(),
+                  ),
+                );
+              },
             ),
+            VisualizationTab(),
           ],
         ),
       ),
