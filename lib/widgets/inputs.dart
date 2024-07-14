39848d976b0b696870cf1209fb878c6319edd7a3
André Pedersen <andrped94@gmail.com>
2024-07-14 21:18:12 +0200

    Allow for creation of new exercises

--- lib/widgets/inputs.dart
+++ lib/widgets/inputs.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../core/database.dart';
-import '../common/constants.dart';
 
 class ExerciseSetter extends StatefulWidget {
   final Function() onExerciseAdded;
@@ -17,22 +16,51 @@ class _ExerciseSetterState extends State<ExerciseSetter> {
   final _exerciseValueController = TextEditingController();
   final DatabaseHelper _dbHelper = DatabaseHelper();
 
-  String? _selectedPredefinedExercise;
+  String? _selectedExercise;
+  bool _isAddingNewExercise = false;
+
+  List<String> _predefinedExercises = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPredefinedExercises();
+  }
+
+  Future<void> _loadPredefinedExercises() async {
+    List<String> exercises = await _dbHelper.getPredefinedExercises();
+    setState(() {
+      _predefinedExercises = exercises;
+      _selectedExercise = _predefinedExercises.isNotEmpty ? _predefinedExercises.first : null;
+    });
+  }
 
   Future<void> _addExercise() async {
     if (_formKey.currentState!.validate()) {
-      final exerciseName = _selectedPredefinedExercise ?? _exerciseNameController.text;
-      // feedback mechanism after inserting exercise
+      final exerciseName = _isAddingNewExercise
+          ? _exerciseNameController.text.trim()
+          : _selectedExercise!;
+      
       await _dbHelper.insertExercise(exerciseName, _exerciseValueController.text);
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text('Exercise added successfully'),
         duration: Duration(seconds: 2),
       ));
+
+      if (_isAddingNewExercise) {
+        await _dbHelper.addPredefinedExercise(exerciseName);
+        setState(() {
+          _predefinedExercises.add(exerciseName);
+          _selectedExercise = exerciseName;
+        });
+      }
+
       _exerciseNameController.clear();
       _exerciseValueController.clear();
       setState(() {
-        _selectedPredefinedExercise = null;
+        _isAddingNewExercise = false;
       });
+
       widget.onExerciseAdded();
     }
   }
@@ -42,55 +70,75 @@ class _ExerciseSetterState extends State<ExerciseSetter> {
     return Form(
       key: _formKey,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            height: 72, // Adjust the height as needed
-            child: DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Select Predefined Exercise'),
-              items: predefinedExercises.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  _selectedPredefinedExercise = value;
-                  _exerciseNameController.clear();
-                });
-              },
-              value: _selectedPredefinedExercise,
-              isExpanded: true,
-              iconSize: 24.0,
-              icon: Icon(Icons.arrow_drop_down),
-              elevation: 16,
-              style: TextStyle(color: Colors.black),
-              validator: (value) {
-                if (_selectedPredefinedExercise == null && (value == null || value.isEmpty)) {
-                  return 'Please enter an exercise name or select one';
-                }
-                return null;
-              },
-              dropdownColor: Colors.white,
-              // Limit the number of items shown
-              selectedItemBuilder: (BuildContext context) {
-                return predefinedExercises.take(dropdownVisibleItemCount).map<Widget>((String value) {
-                  return Text(value);
-                }).toList();
-              },
-              // Allow scrolling to select items outside the visible range
-              // The items are already specified above
-            ),
-          ),
-          TextFormField(
-            controller: _exerciseNameController,
-            decoration: InputDecoration(labelText: 'Exercise Name (or enter custom)'),
-            validator: (value) {
-              if (_selectedPredefinedExercise == null && (value == null || value.isEmpty)) {
-                return 'Please enter an exercise name or select one';
-              }
-              return null;
-            },
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(labelText: 'Select Exercise'),
+                  items: [
+                    ..._predefinedExercises.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }),
+                    DropdownMenuItem<String>(
+                      value: 'custom',
+                      child: Text('Add New Exercise'),
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      if (value == 'custom') {
+                        _isAddingNewExercise = true;
+                        _selectedExercise = null;
+                      } else {
+                        _isAddingNewExercise = false;
+                        _selectedExercise = value;
+                      }
+                      _exerciseNameController.clear();
+                    });
+                  },
+                  value: _selectedExercise,
+                  isExpanded: true,
+                  iconSize: 24.0,
+                  icon: Icon(Icons.arrow_drop_down),
+                  elevation: 16,
+                  style: TextStyle(color: Colors.black),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please select or enter an exercise';
+                    }
+                    if (value == 'custom' && !_isAddingNewExercise) {
+                      return 'Please select or enter an exercise';
+                    }
+                    return null;
+                  },
+                  dropdownColor: Colors.white,
+                ),
+              ),
+              SizedBox(width: 16.0),
+              Expanded(
+                flex: 3,
+                child: TextFormField(
+                  controller: _exerciseNameController,
+                  enabled: _isAddingNewExercise,
+                  decoration: InputDecoration(
+                    labelText: 'Exercise Name (or enter custom)',
+                    enabled: _isAddingNewExercise,
+                  ),
+                  validator: (value) {
+                    if (_isAddingNewExercise && (value == null || value.isEmpty)) {
+                      return 'Please enter an exercise name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            ],
           ),
           TextFormField(
             controller: _exerciseValueController,
