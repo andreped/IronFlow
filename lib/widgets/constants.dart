09fd0f5e9992a90a7d217ed5869c928ec9b6b21a
André Pedersen <andrped94@gmail.com>
2024-07-14 19:13:33 +0200

    Added constants

---
+++ lib/widgets/constants.dart
@@ -0,0 +1,13 @@
+const List<String> predefinedVariables = [
+  "Benchpress",
+  "Pull-up",
+  "Squats",
+  "Curls (z-bar)",
+  "Curls (manuals)",
+  "Deadlift",
+  "Leg extension",
+  "Hamstring curl"
+];
+
+// constants.dart
+const int dropdownVisibleItemCount = 5;
