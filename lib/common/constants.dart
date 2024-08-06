d63125e9fa64c288a740c797c42cf3c5625ce512
André Pedersen <andrped94@gmail.com>
2024-08-06 15:27:54 +0200

    Added more exercises

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,12 +1,24 @@
-List<String> predefinedExercises = [
+List<String> IronFlowExercises = [
   "Benchpress",
   "Pull-up",
   "Squats",
   "Curls (z-bar)",
-  "Curls (manuals)",
+  "Curls (dumbbells)",
   "Deadlift",
-  "Leg extension",
-  "Hamstring curl"
+  "Leg Extension",
+  "Hamstring Curl",
+  "Hip Thrust",
+  "Calf Raises",
+  "Shoulder Press (dumbbells)",
+  "Shoulder Press (bar)",
+  "Front Raises (dumbbells)",
+  "Lateral Raises (dumbbells)",
+  "Scull Crush (z-bar)",
+  "Dips",
+  "Flies (dumbbells)",
+  "Incline Press (dumbbells)",
+  "Incline Press (bar)",
+  "Bulgarian Split (dumbbells)"
 ];
 
 const int dropdownVisibleItemCount = 5;
