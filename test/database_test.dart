18e0a853283b5a1533cbff96740ebdc340a14416
André Pedersen <andrped94@gmail.com>
2024-08-09 22:57:48 +0200

    Fixed linting

--- test/database_test.dart
+++ test/database_test.dart
@@ -28,10 +28,12 @@ void main() {
         options: anyNamed('options'),
       )).thenAnswer((_) async => mockDatabase);
 
-      when(mockDatabase.execute('CREATE TABLE exercises(id INTEGER PRIMARY KEY AUTOINCREMENT, exercise TEXT, weight TEXT, reps INTEGER, sets INTEGER, timestamp TEXT)'))
+      when(mockDatabase.execute(
+              'CREATE TABLE exercises(id INTEGER PRIMARY KEY AUTOINCREMENT, exercise TEXT, weight TEXT, reps INTEGER, sets INTEGER, timestamp TEXT)'))
           .thenAnswer((_) async => Future.value());
 
-      when(mockDatabase.execute('CREATE TABLE IF NOT EXISTS predefined_exercises(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)'))
+      when(mockDatabase.execute(
+              'CREATE TABLE IF NOT EXISTS predefined_exercises(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)'))
           .thenAnswer((_) async => Future.value());
 
       // Act
@@ -126,14 +128,16 @@ void main() {
     test('Should check for new high score correctly', () async {
       // Arrange
       when(mockDatabase.rawQuery(
-        'SELECT weight, reps FROM exercises WHERE exercise = ? ORDER BY weight DESC, reps DESC LIMIT 1', 
-        ['Bench Press']
-      )).thenAnswer((_) async => [
-                {'weight': '100', 'reps': 10}
-              ]);
+          'SELECT weight, reps FROM exercises WHERE exercise = ? ORDER BY weight DESC, reps DESC LIMIT 1',
+          [
+            'Bench Press'
+          ])).thenAnswer((_) async => [
+            {'weight': '100', 'reps': 10}
+          ]);
 
       // Act
-      final isNewHighScore = await databaseHelper.isNewHighScore('Bench Press', 110, 8);
+      final isNewHighScore =
+          await databaseHelper.isNewHighScore('Bench Press', 110, 8);
 
       // Assert
       expect(isNewHighScore, true);
@@ -142,11 +146,9 @@ void main() {
     test('Should calculate total weight for a day correctly', () async {
       // Arrange
       final today = DateTime.now().toString().split(' ')[0];
-      when(mockDatabase.query(
-        'exercises', 
-        where: 'timestamp LIKE ?', 
-        whereArgs: ['$today%']
-      )).thenAnswer((_) async => [
+      when(mockDatabase.query('exercises',
+              where: 'timestamp LIKE ?', whereArgs: ['$today%']))
+          .thenAnswer((_) async => [
                 {
                   'exercise': 'Bench Press',
                   'weight': '100',
@@ -157,7 +159,8 @@ void main() {
               ]);
 
       // Act
-      final totalWeight = await databaseHelper.getTotalWeightForDay(DateTime.now());
+      final totalWeight =
+          await databaseHelper.getTotalWeightForDay(DateTime.now());
 
       // Assert
       expect(totalWeight['Bench Press'], 100.0 * 10 * 4);
@@ -165,13 +168,12 @@ void main() {
 
     test('Should get the last logged exercise correctly', () async {
       // Arrange
-      when(mockDatabase.query(
-        'exercises', 
-        where: 'exercise = ?', 
-        whereArgs: ['Bench Press'], 
-        orderBy: 'timestamp DESC', 
-        limit: 1
-      )).thenAnswer((_) async => [
+      when(mockDatabase.query('exercises',
+              where: 'exercise = ?',
+              whereArgs: ['Bench Press'],
+              orderBy: 'timestamp DESC',
+              limit: 1))
+          .thenAnswer((_) async => [
                 {
                   'exercise': 'Bench Press',
                   'weight': '100',
@@ -182,7 +184,8 @@ void main() {
               ]);
 
       // Act
-      final lastLoggedExercise = await databaseHelper.getLastLoggedExercise('Bench Press');
+      final lastLoggedExercise =
+          await databaseHelper.getLastLoggedExercise('Bench Press');
 
       // Assert
       expect(lastLoggedExercise, isNotNull);
