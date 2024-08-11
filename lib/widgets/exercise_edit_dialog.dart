d27303ab19a8ff5717d7eaf6378be144ddc35926
André Pedersen <andrped94@gmail.com>
2024-08-11 21:30:03 +0200

    Fixes number dialog not being able to close in edit dialog

--- lib/widgets/exercise_edit_dialog.dart
+++ lib/widgets/exercise_edit_dialog.dart
@@ -35,130 +35,135 @@ class _ExerciseEditDialogState extends State<ExerciseEditDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Edit Exercise'),
-      content: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextFormField(
-              controller: _exerciseController,
-              decoration: const InputDecoration(labelText: 'Exercise'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter an exercise';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _weightController,
-              decoration: const InputDecoration(labelText: 'Weight'),
-              keyboardType: TextInputType.text,  // Changed to allow punctuation
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(
-                  RegExp(r'^[\d,.]+$'),  // Allow digits, comma, and period
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+      child: AlertDialog(
+        title: const Text('Edit Exercise'),
+        content: GestureDetector(
+          onTap: () {}, // Prevents dialog from closing when tapping inside
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _exerciseController,
+                  decoration: const InputDecoration(labelText: 'Exercise'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an exercise';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _weightController,
+                  decoration: const InputDecoration(labelText: 'Weight'),
+                  keyboardType: TextInputType.text,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'^[\d,.]+$'),
+                    ),
+                  ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the weight';
+                    }
+                    if (double.tryParse(value.replaceAll(',', '.')) == null || !_isValidWeight(value)) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _repsController,
+                  decoration: const InputDecoration(labelText: 'Reps'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the number of reps';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Please enter a valid integer';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _setsController,
+                  decoration: const InputDecoration(labelText: 'Sets'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the number of sets';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Please enter a valid integer';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _timestampController,
+                  decoration: const InputDecoration(labelText: 'Timestamp (ISO8601)'),
+                  keyboardType: TextInputType.datetime,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a timestamp';
+                    }
+                    try {
+                      DateTime.parse(value);
+                    } catch (e) {
+                      return 'Please enter a valid ISO8601 timestamp';
+                    }
+                    return null;
+                  },
                 ),
               ],
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter the weight';
-                }
-                if (double.tryParse(value.replaceAll(',', '.')) == null || !_isValidWeight(value)) {
-                  return 'Please enter a valid number';
-                }
-                return null;
-              },
             ),
-            TextFormField(
-              controller: _repsController,
-              decoration: const InputDecoration(labelText: 'Reps'),
-              keyboardType: TextInputType.number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter the number of reps';
-                }
-                if (int.tryParse(value) == null) {
-                  return 'Please enter a valid integer';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _setsController,
-              decoration: const InputDecoration(labelText: 'Sets'),
-              keyboardType: TextInputType.number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter the number of sets';
-                }
-                if (int.tryParse(value) == null) {
-                  return 'Please enter a valid integer';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _timestampController,
-              decoration:
-                  const InputDecoration(labelText: 'Timestamp (ISO8601)'),
-              keyboardType: TextInputType.datetime,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a timestamp';
-                }
-                try {
-                  DateTime.parse(value);
-                } catch (e) {
-                  return 'Please enter a valid ISO8601 timestamp';
-                }
-                return null;
-              },
-            ),
-          ],
+          ),
         ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            if (_formKey.currentState!.validate()) {
-              // Ensure weight has a trailing .0 if necessary
-              String weight = _weightController.text.replaceAll(',', '.'); // Handle comma as a decimal
-              double? parsedWeight = double.tryParse(weight);
-              if (parsedWeight != null) {
-                if (parsedWeight == parsedWeight.truncateToDouble()) {
-                  weight = '${parsedWeight.toStringAsFixed(1)}';
-                } else {
-                  weight = parsedWeight.toString();
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                String weight = _weightController.text.replaceAll(',', '.');
+                double? parsedWeight = double.tryParse(weight);
+                if (parsedWeight != null) {
+                  if (parsedWeight == parsedWeight.truncateToDouble()) {
+                    weight = '${parsedWeight.toStringAsFixed(1)}';
+                  } else {
+                    weight = parsedWeight.toString();
+                  }
                 }
-              }
 
-              Navigator.of(context).pop({
-                'id': widget.exerciseData['id'], // Include the id
-                'exercise': _exerciseController.text,
-                'weight': weight,
-                'reps': int.parse(_repsController.text),
-                'sets': int.parse(_setsController.text),
-                'timestamp': _timestampController.text,
-              });
-            }
-          },
-          child: const Text('Save'),
-        ),
-      ],
+                Navigator.of(context).pop({
+                  'id': widget.exerciseData['id'],
+                  'exercise': _exerciseController.text,
+                  'weight': weight,
+                  'reps': int.parse(_repsController.text),
+                  'sets': int.parse(_setsController.text),
+                  'timestamp': _timestampController.text,
+                });
+              }
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      ),
     );
   }
 
-  // Helper method to validate weight input
   bool _isValidWeight(String value) {
-    // Check if the string contains more than one period or any invalid sequences
     if (value.contains('..') || value.contains('..0') || value.endsWith('.')) {
       return false;
     }
