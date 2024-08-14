6edb4b5587399ad666604549fd662fac6d1dda10
André Pedersen <andrped94@gmail.com>
2024-08-15 00:09:17 +0200

    Configuring maestro for integration tests (#160)
    * Added test flow for maestro
    
    * Implemented integration test CI
    
    * Add missing step for maestro installation in CI
    
    * Attempt to fix Maestro CI
    
    * Test using maestro-test-action
    
    * Fix flow path
    
    * Run android integration tests instead of ios for CI
    
    * Updated flow
    
    * Fixed flow script
    
    * Refactored CI
    
    * Updated README to include integration tests section
    
    * Tried to fix CI to use a different approach for iOS
    
    * Ignore report.xml
    
    * Update CI
    
    * Launch app on simulator step before running tests in CI
    
    * Use iPhone 14 Pro for testing
    
    * Try to fix pod install in CI
    
    * Try to fix pod cache
    
    * Use Xcode 15.4 in CI
    
    * Use Xcode 15.4
    
    * Use Xcode 15.2.0 in CI
    
    * Update DEVELOPMENT_TEAM
    
    * Attempt to fix pod issue before app build
    
    * Move pod fix earlier; install cocoapods
    
    * Remove rm Podfile.lock step
    
    * Dont install cocoapods in CI
    
    * Fix path in build
    
    * Wait for simulator to start
    
    * Try fixing maestro in CI
    
    * Wrap maestro CI step inside sh .github/shell/maestro_tests.sh
    
    * Increased driver startup timeout
    
    * Try using MAESTRO_VERSION: 1.37.0 in tests
    
    * Downgrade MAESTRO_VERSION: 1.36.0
    
    * Test MAESTRO_VERSION: 1.37.7
    
    * Use MAESTRO_VERSION: 1.33.1; downgrade macos-12 and xcode
    
    * Downgrade maestro; reduce timeout; try adding loop hack to bash
    
    * Try upgrading maesro; setting timout in bash script
    
    * Reduced sleep from 10 to 2; remove stop maestro step before running tests
    
    * Another try
    
    * Tried removing inputText in flow
    
    * Another try

---
+++ .github/shell/maestro_tests.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+$HOME/.maestro/bin/maestro -v
+i=0
+while [ $i -le 9 ]; do
+pgrep -lf maestro | awk '{print $1}' | xargs -r kill
+sleep 1
+export MAESTRO_DRIVER_STARTUP_TIMEOUT=120000
+$HOME/.maestro/bin/maestro test .maestro/ && break
+let i=i+1
+done
