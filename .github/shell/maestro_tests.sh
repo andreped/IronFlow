d495d0122fecc360e7ee1a0f79815f5e596140f1
André Pedersen <andrped94@gmail.com>
2024-08-14 23:19:24 +0200

    Downgrade maestro; reduce timeout; try adding loop hack to bash

--- .github/shell/maestro_tests.sh
+++ .github/shell/maestro_tests.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
-export MAESTRO_DRIVER_STARTUP_TIMEOUT=3000000
+export MAESTRO_DRIVER_STARTUP_TIMEOUT=60000
 $HOME/.maestro/bin/maestro -v
-$HOME/.maestro/bin/maestro test .maestro/ --format junit
+i=0
+while [ $i -le 9 ]; do
+pgrep -lf maestro | awk '{print $1}' | xargs -r kill
+sleep 1
+$HOME/.maestro/bin/maestro test .maestro/ && break
+let i=i+1
+done
