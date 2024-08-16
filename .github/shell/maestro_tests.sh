5e16dab2bc353fa82d07ff8585edeb68aa4115f5
André Pedersen <andrped94@gmail.com>
2024-08-16 14:49:17 +0200

    Try to use revised flow in CI

--- .github/shell/maestro_tests.sh
+++ .github/shell/maestro_tests.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 $HOME/.maestro/bin/maestro -v
 i=0
-while [ $i -le 9 ]; do
+while [ $i -le 2 ]; do
 pgrep -lf maestro | awk '{print $1}' | xargs -r kill
 sleep 1
 export MAESTRO_DRIVER_STARTUP_TIMEOUT=120000
-$HOME/.maestro/bin/maestro test .maestro/inputs_tab_save_flow.yml && break
+$HOME/.maestro/bin/maestro test .maestro/inputs_tab_CI_flow.yml && break
 let i=i+1
 done
