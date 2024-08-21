4bcb444036015ab3d03e6c06b4dd7442983da66c
André Pedersen <andrped94@gmail.com>
2024-08-21 21:56:53 +0200

    Remove old comment in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'tabs/home.dart';
-import 'core/theme.dart';
+import 'core/theme.dart'; // Import your AppThemes
 
 void main() {
   runApp(const MyApp());
