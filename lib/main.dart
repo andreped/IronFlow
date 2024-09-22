b1216d9bd14ab75277650e664653ca3ed8ac9f6d
André Pedersen <andrped94@gmail.com>
2024-09-22 19:38:25 +0200

    Settings tab now remember past set values

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'tabs/home.dart';
 import 'core/theme.dart';
 
@@ -17,16 +18,38 @@ class _MyAppState extends State<MyApp> {
   AppTheme _appTheme = AppTheme.system; // Default to system theme
   bool _isKg = true; // Default unit system
 
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _appTheme = AppTheme.values[prefs.getInt('appTheme') ?? _appTheme.index];
+      _isKg = prefs.getBool('isKg') ?? _isKg;
+    });
+  }
+
+  Future<void> _saveSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('appTheme', _appTheme.index);
+    await prefs.setBool('isKg', _isKg);
+  }
+
   void _toggleTheme(AppTheme newTheme) {
     setState(() {
       _appTheme = newTheme;
     });
+    _saveSettings();
   }
 
   void _toggleUnit(bool newUnit) {
     setState(() {
       _isKg = newUnit;
     });
+    _saveSettings();
   }
 
   @override
