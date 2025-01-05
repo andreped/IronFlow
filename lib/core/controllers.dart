5c67c1c6e61518fe9d279e274c8c05950f0221eb
André Pedersen <andrped94@gmail.com>
2025-01-05 22:50:58 +0100

    Fixed bug where new chunks were not auto-synced horizontally (#394)

--- lib/core/controllers.dart
+++ lib/core/controllers.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class ClampingScrollController extends ScrollController {
   @override
   void jumpTo(double value) {
-    final double maxScrollExtent = position.maxScrollExtent;
-    final double minScrollExtent = position.minScrollExtent;
-    final double clampedValue = value.clamp(minScrollExtent, maxScrollExtent);
-    super.jumpTo(clampedValue);
+    if (hasClients) {
+      final double maxScrollExtent = position.maxScrollExtent;
+      final double minScrollExtent = position.minScrollExtent;
+      final double clampedValue = value.clamp(minScrollExtent, maxScrollExtent);
+      super.jumpTo(clampedValue);
+    }
   }
 
   @override
@@ -15,9 +17,13 @@ class ClampingScrollController extends ScrollController {
     required Duration duration,
     required Curve curve,
   }) {
-    final double maxScrollExtent = position.maxScrollExtent;
-    final double minScrollExtent = position.minScrollExtent;
-    final double clampedOffset = offset.clamp(minScrollExtent, maxScrollExtent);
-    return super.animateTo(clampedOffset, duration: duration, curve: curve);
+    if (hasClients) {
+      final double maxScrollExtent = position.maxScrollExtent;
+      final double minScrollExtent = position.minScrollExtent;
+      final double clampedOffset =
+          offset.clamp(minScrollExtent, maxScrollExtent);
+      return super.animateTo(clampedOffset, duration: duration, curve: curve);
+    }
+    return Future.value();
   }
 }
