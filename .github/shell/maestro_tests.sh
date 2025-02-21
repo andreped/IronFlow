ad274db51f2156bfbc7cc0d5d3a971f50a521568
André Pedersen <andrped94@gmail.com>
2025-02-21 09:05:33 +0100

    Skip maestro retries

--- .github/shell/maestro_tests.sh
+++ .github/shell/maestro_tests.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 export MAESTRO_DRIVER_STARTUP_TIMEOUT=120000
 $HOME/.maestro/bin/maestro -v
-i=0
-while [ $i -le 2 ]; do
+#i=0
+#while [ $i -le 2 ]; do
 #pgrep -lf maestro | awk '{print $1}' | xargs -r kill
 #sleep 1
-$HOME/.maestro/bin/maestro test .maestro/integration_tests.yml && break
-let i=i+1
-done
+$HOME/.maestro/bin/maestro test .maestro/integration_tests.yml
+# && break
+#let i=i+1
+#done
